64381be342edb0baf1bd97d75b816641e1f3320f
ejonakodra <ejonako@gmail.com>
2023-02-23 10:15:44 -0800

    function returns sum of two matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -9,5 +9,6 @@ def add_matrices2D(mat1, mat2):
     if len(mat1[0]) != len(mat2[0]):
         return None
     r = [[mat1[i][j] + mat2[i][j]
-          for j in range(len(mat1[0]))] for i in range(len(mat1))]
+          for j in range(len(mat1[0]))]
+         for i in range(len(mat1))]
     return r
