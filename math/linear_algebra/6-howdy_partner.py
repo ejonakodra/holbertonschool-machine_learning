2b9a910704275b1c4ba532ffc76fcdc4888c57f8
ejonakodra <ejonako@gmail.com>
2023-02-23 10:25:25 -0800

    function concatinates 2 arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function concatinates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """concat the arrays and return the result"""
+    cat_arrays = arr1 + arr2
+    return cat_arrays
