1dc1a99b3c653cc67d83f011ac48cd63b7ab7c2a
ejonakodra <ejonako@gmail.com>
2023-02-24 05:02:31 -0800

    function calculates sum of 2 matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """function returns the sum of two matrix"""
 
+
 def add_matrices2D(mat1, mat2):
-    """check if len mat1 and let mat2 match then rertun the sum matrix"""
-    mat = []
-    if len(mat1[0]) != len(mat2[0]) or len(mat1) != len(mat2):
+    """check if mat1 and mat2 does not have the same shape else return the sum"""
+    if len(mat1) != len(mat2):
         return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    mat = []
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat1[0])):
             row.append(mat1[i][j] + mat2[i][j])
-            mat.append(row)
+        mat.append(row)
     return mat
-                                                                                                                                 
     
