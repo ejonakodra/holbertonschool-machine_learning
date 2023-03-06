950d3c595763de61b848a87e6c67426319fd0cbd
ejonakodra <ejonako@gmail.com>
2023-03-06 06:21:16 -0800

    function calculated summation_i_squared

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,5 +6,5 @@ def summation_i_squared(n):
     """check if n is valid, then return n"""
     if type(n) is not int or n < 1:
         return None
-    sigma_sum = (n * (n + 1) * ((2 * n) + 1)) / 6
-    return int(sigma_sum)            
+    sigma_sum = (n * (n + 1) * ((2 * n) + 1))/6
+    return int(sigma_sum)
