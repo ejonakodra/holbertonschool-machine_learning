0e637cdd6f1bca08c54de494e21c4bbdba4e5996
ejonakodra <ejonako@gmail.com>
2023-03-06 12:34:47 -0800

    function calculats the derivate of a polynom

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,7 +6,7 @@ def poly_derivative(poly):
     """"check if poly is valid then find the derivate"""
     if(type(poly) is not list or poly == []):
         return None
-    elif len(poly) <2:
+    elif len(poly) < 2:
         return [0]
     else:
         derivative = poly.copy()
