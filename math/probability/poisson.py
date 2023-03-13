537cdf8600dfd4fae684fcd43512f142606e0bbf
ejonakodra <ejonako@gmail.com>
2023-03-13 07:06:04 -0700

    Create Poission class and initialize lambtha

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -4,7 +4,6 @@
 
 class Poisson:
     """    class that represents Poisson distribution """
-
     def __init__(self, data=None, lambtha=1.):
         """initialize lambda, 
         if data is given use the lambtha is it is positive
