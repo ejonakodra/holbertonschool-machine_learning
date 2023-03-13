4bcbffebc6afee106c933c83c5b54dca87f16e1f
ejonakodra <ejonako@gmail.com>
2023-03-13 07:35:18 -0700

    CDF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -36,12 +36,13 @@ class Poisson:
         pmf = ((lambtha ** k) * (e ** -lambtha)) / factorial
         return pmf
 
-       def cdf(self, k):
-           """calculates the value of the CDF for a given number of successes k"""
-           if type(k) is not int:
-               k = int(k)
-            if k < 0:
-                return 0
-            for i in range(k + 1):
-                cdf += self.pmf(i)
-            return cdf
+    def cdf(self, k):
+    """calculates the value of the CDF for a given number of successes k"""
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
