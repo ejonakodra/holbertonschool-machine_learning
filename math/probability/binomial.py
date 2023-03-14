d2343b219c9b10296575f6d45f56fea1abb6c2f6
ejonakodra <ejonako@gmail.com>
2023-03-14 14:04:37 -0700

    Calculate CDF value -binomial distribution

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -54,3 +54,14 @@ class Binomial:
         binomial_co = n_factorial / (k_factorial * nk_factorial)
         pmf = binomial_co * (p ** k) * (q ** (n - k))
         return pmf
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of k"""
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
