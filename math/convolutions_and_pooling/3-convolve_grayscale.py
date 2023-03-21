b83a49c9a5295f533f2b3eab1062fceecb68ec15
ejonakodra <ejonako@gmail.com>
2023-03-21 08:47:40 -0700

    Function performs convolution on a grayscale image with given padding a stride

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -20,18 +20,18 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         pw = 0
     else:
         ph, pw = padding
-        images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
-                        'constant', constant_values=0)
-        ch = ((height + (2 * ph) - kh) // sh) + 1
-        cw = ((width + (2 * pw) - kw) // sw) + 1
-        convoluted = np.zeros((m, ch, cw))
-        i = 0
-        for h in range(0, (height + (2 * ph) - kh + 1), sh):
-            j = 0
-            for w in range(0, (width + (2 * pw) - kw + 1), sw):
-                output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
-                                axis=1).sum(axis=1)
-                convoluted[:, i, j] = output
-                j += 1
-            i += 1
-        return convoluted
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                    'constant', constant_values=0)
+    ch = ((height + (2 * ph) - kh) // sh) + 1
+    cw = ((width + (2 * pw) - kw) // sw) + 1
+    convoluted = np.zeros((m, ch, cw))
+    i = 0
+    for h in range(0, (height + (2 * ph) - kh + 1), sh):
+        j = 0
+        for w in range(0, (width + (2 * pw) - kw + 1), sw):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
+                            axis=1).sum(axis=1)
+            convoluted[:, i, j] = output
+            j += 1
+        i += 1
+    return convoluted
