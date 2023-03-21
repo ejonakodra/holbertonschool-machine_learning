52bd05d7dc103b6924a3255448c5b8988a2d3a32
ejonakodra <ejonako@gmail.com>
2023-03-21 14:02:49 -0700

    Function performs convolution on images with multiple channels and multiple kernels using given padding and stride

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -21,7 +21,7 @@ def convolve(images, kernel, padding='same', stride=(1, 1)):
     else:
         ph, pw = padding
     images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        'constant', constant_values=0)
+                    'constant', constant_values=0)
     ch = ((height + (2 * ph) - kh) // sh) + 1
     cw = ((width + (2 * pw) - kw) // sw) + 1
     convoluted = np.zeros((m, ch, cw, nc))
