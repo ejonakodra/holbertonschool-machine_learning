d132ffe7190d7574ec5f616c82748669b57d57fa
ejonakodra <ejonako@gmail.com>
2023-03-21 07:12:18 -0700

    Function performs convolution on a grayscale image with custom padding

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -12,7 +12,7 @@ def convolve_grayscale_padding(images, kernel, padding):
     kh, kw = kernel.shape
     ph, pw = padding
     images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
-                    'constant', constant_values = 0)
+                    'constant', constant_values=0)
     ch = height + (2*ph) - kh + 1
     cw = width + (2*pw) - kw + 1
     convoluted = np.zeros((m, ch, cw))
