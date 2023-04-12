05ca8893f9913caaf3da6f9ce1e4f89eedbabbd6
ejonakodra <ejonako@gmail.com>
2023-04-12 06:21:52 -0700

    Class representing single neuron that performs binary classification

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -22,9 +22,11 @@ class Neuron:
     @property
     def W(self):
         return (self.__W)
+    
     @property
     def b(self):
         return (self.__b)
+    
     @property
     def A(self):
         return (self.__A)
