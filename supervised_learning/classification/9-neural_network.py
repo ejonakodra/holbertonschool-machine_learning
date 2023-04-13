e39cf4e8afd9f0c08723e7c0f613ed0bc6a74078
ejonakodra <ejonako@gmail.com>
2023-04-13 09:16:29 -0700

    Add new version of Nerual Network class with private attributes

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -28,26 +28,26 @@ class NeuralNetwork:
         self.__b2 = 0
         self.__A2 = 0
 
-        @property
-        def W1(self):
-            return (self.__W1)
+    @property
+    def W1(self):
+        return (self.__W1)
 
-        @property
-        def b1(self):
-            return (self.__b1)
+    @property
+    def b1(self):
+        return (self.__b1)
 
-        @property
-        def A1(self):
-            return (self.__A1)
+    @property
+    def A1(self):
+        return (self.__A1)
 
-        @property
-        def W2(self):
-            return (self.__W2)
+    @property
+    def W2(self):
+        return (self.__W2)
 
-        @property
-        def b2(self):
-            return (self.__b2)
+    @property
+    def b2(self):
+        return (self.__b2)
 
-        @property
-        def A2(self):
-            return (self.__A2)
+    @property
+    def A2(self):
+        return (self.__A2)
