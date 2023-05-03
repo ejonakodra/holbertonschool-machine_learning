5da26cebceacbadb2015743fbdea92f43a39a023
ejonakodra <ejonako@gmail.com>
2023-05-03 07:04:28 -0700

    Add function that shuffles the data-points in two matrices the same way

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Function shuffles the data point in two matrices the same way"""
 
+import numpy as np
+
 
 def shuffle_data(X, Y):
     """Shuffles the data points in two matrices the same way"""
