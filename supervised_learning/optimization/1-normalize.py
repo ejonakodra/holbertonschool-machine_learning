d61f40cf49e72f0505423e438e75bd056a6fda31
ejonakodra <ejonako@gmail.com>
2023-05-03 05:08:28 -0700

    Add function that normalized a matrix

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -3,7 +3,8 @@
 
 
 def normalize(X, m, s):
-    """Normalize X by subtracting the mean and dividing by the standard deviation"""
+    """Normalize X by subtracting the mean
+    and dividing by the standard deviation"""
     X_norm = (X - m) / s
 
     return X_norm
