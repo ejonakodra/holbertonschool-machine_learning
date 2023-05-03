9ade7ed36074301975b0fb785a947c56ef241d07
ejonakodra <ejonako@gmail.com>
2023-05-03 07:20:05 -0700

    Fix return indentation

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Function that trains a loaded neural network model using mini-batch gradient descent"""
+"""Function that trains a loaded neural network model
+using mini-batch gradient descent"""
 
 
 import tensorflow as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,epochs=5,
-                     load_path="/tmp/model.ckpt",save_path="/tmp/model.ckpt"):
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5,
+                     load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
     """Trains a loaded neural network model using mini-batch gradient descent"""
     m = X_train.shape[0]
     with tf.Session() as sess:
@@ -21,13 +23,13 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,epochs=5,
 
         for epoch in range(epochs + 1):
             print("After {} epochs:".format(epoch))
-            train_cost = sess.run(loss, feed_dict={x: X_train,y: Y_train})
+            train_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
             print("\tTraining Cost: {}".format(train_cost))
-            train_accuracy = sess.run(accuracy, feed_dict={x: X_train,y: Y_train})
+            train_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
             print("\tTraining Accuracy: {}".format(train_accuracy))
-            valid_cost = sess.run(loss, feed_dict={x: X_valid,y: Y_valid})
+            valid_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
             print("\tValidation Cost: {}".format(valid_cost))
-            valid_accuracy = sess.run(accuracy, feed_dict={x: X_valid,y: Y_valid})
+            valid_accuracy = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
             print("\tValidation Accuracy: {}".format(valid_accuracy))
             if epoch == epochs:
                 break
@@ -46,8 +48,10 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,epochs=5,
                 step_number += 1
                 if (step_number % 100) is 0:
                     print("\tStep {}:".format(step_number))
-                    step_cost = sess.run(loss,feed_dict={x: X_train_s[low:high, :],y: Y_train_s[low:high, :]})
+                    step_cost = sess.run(loss,
+                                         feed_dict={x: X_train_s[low:high, :],y: Y_train_s[low:high, :]})
                     print("\t\tCost: {}".format(step_cost))
-                    step_accuracy = sess.run(accuracy,feed_dict={x: X_train_s[low:high, :],y: Y_train_s[low:high, :]})
+                    step_accuracy = sess.run(accuracy,
+                                             feed_dict={x: X_train_s[low:high, :],y: Y_train_s[low:high, :]})
                     print("\t\tAccuracy: {}".format(step_accuracy))
         return (saver.save(sess, save_path))
