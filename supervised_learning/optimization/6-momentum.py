c46d01f489e514ccbecd84aab8f8d167ec36a30c
ejonakodra <ejonako@gmail.com>
2023-05-04 07:23:32 -0700

    Add function that creates training op for a NN with TF

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -4,9 +4,8 @@ for a neural network in TensorFlow using
 the gradient descent with momentum optimization algorithm"""
 
 
-def update_variables_momentum(alpha, beta1, var, grad, v):
+def create_momentum_op(loss, alpha, beta1):
     """Creates the training operation for a neural network in TensorFlow
     using the gradient descent with momentum optimization algorithm """
-    dW_prev = (beta1 * v) + ((1 - beta1) * grad)
-    var -= (alpha * dW_prev)
-    return var, dW_prev
+    op = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
+    return op
