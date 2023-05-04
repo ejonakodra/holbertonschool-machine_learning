b175f61314141e80a4b87d91f2841125d6beac13
ejonakodra <ejonako@gmail.com>
2023-05-04 07:38:25 -0700

    Add function that updates a var using Adam Opt algo

---
+++ supervised_learning/optimization/9-Adam.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Function that updates a variable in place
+using the Adam optimization algorithm"""
+
+def update_variables_Adam(alpha, beta1, beta2,
+                          epsilon, var, grad, v, s, t):
+    """Updates a variable in place using
+    Adam optimization algorithm"""
+    v_dW = (beta1 * v) + ((1 - beta1) * grad)
+    s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    v_dW_c = v_dW / (1 - (beta1 ** t))
+    s_dW_c = s_dW / (1 - (beta2 ** t))
+    var -= alpha * (v_dW_c / (epsilon + (s_dW_c ** (1 / 2))))
+    return var, v_dW, s_dW
