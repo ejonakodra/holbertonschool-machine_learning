054e4dbf6e201c6fb42644a43bcf5355fee4565f
ejonakodra <ejonako@gmail.com>
2023-05-08 15:06:58 -0700

    Add function that calculates precision

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -6,9 +6,9 @@ import numpy as np
 
 
 def precision(confusion):
-    """function returns numpy.ndarray of shape (classes,) 
+    """function returns numpy.ndarray of shape (classes,)
     containing precision of each class"""
     tp = np.diag(confusion)
-    fp = np.sum(confusion, axis=0) - tp    
+    fp = np.sum(confusion, axis=0) - tp
     precision = tp / (tp + fp)
     return precision
