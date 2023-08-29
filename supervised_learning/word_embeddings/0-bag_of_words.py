25eabd8921bb799a79a9b8124682beb4b8baab47
ejonakodra <80434041+ejonakodra@users.noreply.github.com>
2023-08-29 16:31:22 +0200

    Update 0-bag_of_words.py
    new commit for bag of words

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
-"""
-Defines function that creates a bag of words embedding matrix
-"""
+"""Bag of words"""
 
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
-    """Creates a bag of words embedding matrix
-
-    parameters:
-        sentences [list]:
-            list of sentences to analyze
-
-        vocab [list]:
-            list of vocabulary words to use for analysis
-            if None, all words within sentences should be used
-
-    returns:
-        embeddings,features:
-            embeddings [numpy.ndarray of shape (s, f)]:
-                contains the embeddings
-                s: number of sentences in sentences
-                f: number of features analyzed
-            features [list]:
-                list of features used for embeddings"""
+    """
+    bag of words function
+    Args:
+        sentences: list of sentences to analize
+        vocab: list of the vocabulary words to use for the analysis
+    Returns: embeddings, features
+    """
     if vocab is None:
-        vocab = set(word for sentence in sentences for word in sentence.split())
-    
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    embeddings = vectorizer.transform(sentences).toarray()
-    features = vectorizer.get_feature_names()
-    
-    return embeddings, features
+        vectorizer = CountVectorizer()
+        X = vectorizer.fit_transform(sentences)
+        vocab = vectorizer.get_feature_names()
+    else:
+        vectorizer = CountVectorizer(vocabulary=vocab)
+        X = vectorizer.fit_transform(sentences)
+    embedding = X.toarray()
+
+    return embedding, vocab
