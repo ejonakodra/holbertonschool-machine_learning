d4b5909d8effddd78a82d3dd3840ec31160a5526
ejonakodra <80434041+ejonakodra@users.noreply.github.com>
2023-08-30 15:37:58 +0200

    Update 6-dropout_create_layer.py

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-"""
-Initial placeholder
-"""
+"""Imports tensorflow"""
 
 import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     """
-    Initial placeholder
+    dropout_create_layer
     """
-    pass
+    dropout = tf.layers.Dropout(keep_prob)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    tensor = tf.layers.Dense(units=n, activation=activation,
+                             kernel_initializer=init,
+                             kernel_regularizer=dropout)
+    return tensor(prev)
