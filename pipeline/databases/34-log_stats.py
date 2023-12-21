6aa0cb0fe87d298ad335e261de3f4cbfadbfcb6c
Ejona Kodra <80434041+ejonakodra@users.noreply.github.com>
2023-12-21 12:17:49 +0100

    log stats

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python3
 """
-script that provides some stats about Nginx logs stored in MongoDB:
-
-- Database: logs
-- Collection: nginx
-- Display (same as the example):
-- first line: x logs where x is the number of documents in this collection
-- second line: Methods:
-- 5 lines with the n° of documents with the method = ["GET", "POST", "PUT",
-  "PATCH", "DELETE"] in this order (see example below - warning:
-  it’s a tabulation before each line)
-- one line with the number of documents with:
-- method=GET
-- path=/status
+Provides some stats about Nginx logs stored in MongoDB
 """
+
+
 from pymongo import MongoClient
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """
+    Gets stats about Nginx logs stored in MongoDB
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-    boundaries = ["DELETE", "GET", "PATCH", "POST", "PUT"]
-    aggregation = {'$bucket': {'groupBy': '$method',
-                               'boundaries': boundaries,
-                               'output': {'total': {'$sum': 1}}}}
-    methods = collection.aggregate([aggregation])
-    methods = {x.get('_id'): x.get('total') for x in methods}
-    boundaries = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(collection.count_documents({}), 'logs')
+    logs_coll = client.logs.nginx
+    doc_count = logs_coll.count_documents({})
+    print("{} logs".format(doc_count))
     print("Methods:")
-    for b in boundaries:
-        print('\tmethod {}:'.format(b),
-              methods.get(b) if methods.get(b) else 0)
-    check = collection.aggregate([{'$match': {'path': '/status'}},
-                                  {'$count': "check"}])
-    check = list(check._CommandCursor__data)[0].get('check')
-    print(check, 'status check')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = logs_coll.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    filter_path = {"method": "GET", "path": "/status"}
+    path_count = logs_coll.count_documents(filter_path)
+    print("{} status check".format(path_count))
